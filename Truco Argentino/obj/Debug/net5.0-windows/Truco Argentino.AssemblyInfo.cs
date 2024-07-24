c8051350da15bc4e712f00ef202764c0f3eb2e1b
Fabrizio <fabriziomaggia@gmail.com>
2024-07-23 21:25:39 -0300

    nuget

--- Truco Argentino/obj/Debug/net5.0-windows/Truco Argentino.AssemblyInfo.cs
+++ Truco Argentino/obj/Debug/net5.0-windows/Truco Argentino.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Truco Argentino")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d5ede03f95dff8420a040c4457531acf32d469")]
 [assembly: System.Reflection.AssemblyProductAttribute("Truco Argentino")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Truco Argentino")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
